55b5d3b5cd7bf4a290dac9bb98541786e4b9efd6
Sandeep Kumar <>
2022-07-12 14:27:36 +0530

    Creating new branch for server to deploy on heroku

--- client/src/components/loader/loader.tsx
+++
@@ -1,9 +0,0 @@
-const Loader = function () {
-  return (
-    <div className="spinner-border text-primary loader" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-  );
-};
-
-export default Loader;
